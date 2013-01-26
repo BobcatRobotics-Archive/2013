612c51b8a3a03148b756d37da94a8d53af1b85ad
Schroeder <>
2013-01-26 15:41:06 -0500

    Update park algorithm in AutoMode.java.

--- 177Robot/src/edu/wpi/first/wpilibj/templates/Climber.java
+++ 177Robot/src/edu/wpi/first/wpilibj/templates/Climber.java
@@ -5,72 +5,85 @@
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  * @author Robotics
  */
 public class Climber extends Thread {
-    
+    //constants 
+    private static final int DRIVING = 0;
+    private static final int RETRACTING = 1;
+    private static final int EXTENDING = 2;
+    private static final double throttle = 0.5;
+    private static final double stagelength = 36;
+    private static final double encoderThresh = 2;
     
     Team177Robot robot;
-    double throttle = 0.5;
-    final int DRIVING = 0;
-    final int RETRACTING = 1;
-    final int EXTENDING = 2;
-    int state = DRIVING;
-    boolean enabled = true;
-    
-    final double stagelength = 36;
-    final double encoderThresh = 2;
-    
+    private int state = DRIVING;
+    private boolean enabled = true;
+
     /* Limit Switches */
-    DigitalInput lowerlimit = new DigitalInput(8);
-    DigitalInput upperlimit = new DigitalInput(9);
+    private DigitalInput lowerlimit = new DigitalInput(8);
+    private DigitalInput upperlimit = new DigitalInput(9);
     
     Climber(Team177Robot robot) {
         this.robot = robot;
     }
     
-    
     public void run() {
-        if(enabled) {
-        switch(state) {
-            case RETRACTING:
-                Retracting();
-            case EXTENDING:
-                Extending();
-            case DRIVING:
-                Driving();
-         }
-        } else {
-            robot.drive.tankDrive(0, 0);
-            //TODO: Fire pneumatic brake
+        
+        while (true) {
+            SmartDashboard.putBoolean("Climber Enable", enabled);
+            if (enabled) {
+                switch (state) {
+                    case RETRACTING:
+                        SmartDashboard.putString("Climber State", "Retracting");
+                        Retracting();
+                    case EXTENDING:
+                        SmartDashboard.putString("Climber State", "Extending");
+                        Extending();
+                    case DRIVING:
+                        SmartDashboard.putString("Climber State", "Driving");
+                        Driving();
+                }
+            } else {
+                robot.drive.tankDrive(0, 0);
+                //TODO: Fire pneumatic brake
+            }
+            
+            /* Sleep for a while */
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {}
         }
-                      
-     }
+    }
     
-    public void Extending() {
+    private void Extending() {
         robot.drive.tankDrive(throttle, throttle);
         double maxdistance = Math.max(robot.locator.getLeftRaw(), robot.locator.getRightRaw());
-        if((Math.abs(maxdistance-stagelength) < encoderThresh) || (upperlimit.get())) {
+        if((Math.abs(maxdistance-stagelength) < encoderThresh) || (!upperlimit.get())) {
             state = RETRACTING;
         }
     }
     
-    public void Retracting() {
+    private void Retracting() {
         robot.drive.tankDrive(-throttle, -throttle);
         double mindistance = Math.min(robot.locator.getLeftRaw(), robot.locator.getRightRaw());
-        if((mindistance < encoderThresh) || (lowerlimit.get())) {
+        if((mindistance < encoderThresh) || (!lowerlimit.get())) {
             state = EXTENDING;
         }
         
     }
     
-    public void Driving() {
+    private void Driving() {
         //TODO: Check for pyramid contact
         robot.locator.Reset();
         state = EXTENDING;
     }
-
+    
+    public synchronized void enable(boolean e) {
+        enabled = e;
+    }
 }

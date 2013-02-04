e2e0944e94a6e5596f63a01e457c825f8869520a
David Schroeder <kb1fch@gmail.com>
2013-02-04 15:30:38 -0500

    Move LiveWindow stuff to main robot class. Add elevation logic.

--- 177Robot/src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ 177Robot/src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -10,10 +10,7 @@ import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-
 
 /**
  * Shooter Control Logic
@@ -24,28 +21,38 @@ public class Shooter extends Thread {
     //Timing
     private static final double shooterTimeOut = 5.0;  //seconds to try and reach speed
     private static final double feedTime = 0.5;  //seconds to keep wheel spinning after actuating the feed mechanism
+    private static final double pinTime = 0.01;  //seconds to delay between pulling restraining pin and feeding 
     private static final boolean shootOnTimeout = true; //shoot after shooterTimeOut seconds, even if not at speed
+    
+    //Speed setpoints
+    private static final double ElevatedSetpoint1 = 3000;
+    private static final double ElevatedSetpoint2 = 3000;
+    private static final double NonElevatedSetpoint1 = 3000;
+    private static final double NonElevatedSetpoint2 = 3000;
     
     // Mode Constants
     private static final int STANDBY = 0;
     private static final int SPINUP = 1;
     private static final int FEED = 2;
     
-    private final Encoder shooterEncoder1;
-    private final ShooterMotor shooterMotor1;
-    private PIDController shooterControl1;
+    final Encoder shooterEncoder1;
+    final ShooterMotor shooterMotor1;
+    PIDController shooterControl1;
     
-    private final Encoder shooterEncoder2;
-    private final ShooterMotor shooterMotor2;
-    private PIDController shooterControl2;
+    final Encoder shooterEncoder2;
+    final ShooterMotor shooterMotor2;
+    PIDController shooterControl2;
     
-    private final Solenoid shooterFeed;
+    final Solenoid shooterFeed;
+    final Solenoid shooterPin;
+    final Solenoid shooterElevation;
     
     private int shooterMode;
+    private boolean elevated = false;
     private boolean spinTest = false;
     private boolean feedTest = false;
     
-    public Shooter(int Motor1, int Encoder1A, int Encoder1B, int Motor2, int Encoder2A, int Encoder2B, int Feed) {
+    public Shooter(int Motor1, int Encoder1A, int Encoder1B, int Motor2, int Encoder2A, int Encoder2B, int Feed, int Pin, int Elevation) {
        shooterEncoder1 = new Encoder(Encoder1A,Encoder1B);
        shooterEncoder1.setDistancePerPulse(60.0/360.0); //360 pulse per revolution, multiplied by 60 to RPM? - need to confirm.
        shooterEncoder1.setPIDSourceParameter(Encoder.PIDSourceParameter.kRate);
@@ -54,35 +61,32 @@ public class Shooter extends Thread {
        shooterEncoder2.setDistancePerPulse(60.0/360.0); //360 pulse per revolution, multiplied by 60 to RPM? - need to confirm.
        shooterEncoder2.setPIDSourceParameter(Encoder.PIDSourceParameter.kRate);
        
-       shooterMotor1 = new ShooterMotor(Motor1, "Motor 1");
-       shooterMotor2 = new ShooterMotor(Motor2, "Motor 2");
+       shooterMotor1 = new ShooterMotor(Motor1);
+       shooterMotor2 = new ShooterMotor(Motor2);
        
        shooterFeed = new Solenoid(Feed);
+       shooterPin = new Solenoid(Pin);
+       shooterElevation = new Solenoid(Elevation);
        
        shooterControl1 = new PIDController(0.001, 0, 0, 0.5/3000.0, shooterEncoder1, shooterMotor1); 
-       shooterControl1.setAbsoluteTolerance(250.0); //set Tolerance to +/- 250 RPM
-       shooterControl1.setSetpoint(3000);   //This is just a swag
+       shooterControl1.setAbsoluteTolerance(100.0); //set Tolerance to +/- 100 RPM
+       shooterControl1.setSetpoint(NonElevatedSetpoint1);
        shooterControl1.disable();
        
        shooterControl2 = new PIDController(0.001, 0, 0, 0.5/3000.0, shooterEncoder2, shooterMotor2); 
-       shooterControl2.setAbsoluteTolerance(250.0); //set Tolerance to +/- 250 RPM
-       shooterControl2.setSetpoint(3000);   //This is just a swag
+       shooterControl2.setAbsoluteTolerance(100.0); //set Tolerance to +/- 100 RPM
+       shooterControl2.setSetpoint(NonElevatedSetpoint2);
        shooterControl2.disable();
-       
-       LiveWindow.addActuator("Shooter", "Encoder 1", shooterEncoder1);
-       LiveWindow.addActuator("Shooter", "Encoder 2", shooterEncoder2);
-       LiveWindow.addActuator("Shooter", "Feed", shooterFeed);
-       
+                    
        shooterMode = STANDBY;
     }    
     
-    private class ShooterMotor implements PIDOutput {
+    class ShooterMotor implements PIDOutput {
         
-        private final Victor shooterMotor;
+        final Victor shooterMotor;
         
-        public ShooterMotor(int Motor, String Name) {
-            shooterMotor = new Victor(Motor);
-            LiveWindow.addActuator("Shooter", Name, shooterMotor);
+        public ShooterMotor(int Motor) {
+            shooterMotor = new Victor(Motor);            
         }
         
         public void pidWrite(double output) {
@@ -91,15 +95,16 @@ public class Shooter extends Thread {
     }
     
     public void run() {
-        boolean shooting = false;
-        double shootTime = 0;
-        boolean shooterOn = false;
-        
+        boolean feed = false;  //flag to indicate feeder should be actuated
+        boolean spin = false;  //flag to indicate shooter wheels should be spining
+        boolean pin = false;   //flag to indicate pin should be pulled
+        double shootTime = 0;  //Keep track of sequence timing
+                
         while(true) {
             switch(shooterMode) {
                 case SPINUP:
-                    if (!shooterOn) {
-                        shooterOn = true;
+                    if (!spin) {
+                        spin = true;
                         shootTime = Timer.getFPGATimestamp(); //start timeout timer
                     }
                     
@@ -114,32 +119,36 @@ public class Shooter extends Thread {
                             //Fire anyway 
                             shooterMode = FEED;                      
                         } else {
-                            shooterOn = false;
+                            spin = false;
                             shooterMode = STANDBY;
                         }
                     }
                     break;
                 case FEED:
-                    if(shooting == false) {
-                        shooting = true;
+                    if(pin == false) {
+                        pin = true;
                         shootTime = Timer.getFPGATimestamp();
-                    } else if ((Timer.getFPGATimestamp() - shootTime) > feedTime) {
+                    } else if ((Timer.getFPGATimestamp() - shootTime) > feedTime+pinTime) {
                         /* Delay to give shooter time to complete the shot */
                         //TODO - Add logic to deal with multiple frizbees
-                        shooterFeed.set(false);
-                        shooting = false;
+                        feed = false;
+                        pin = false;
                         shooterMode = STANDBY;
-                    }                                                  
+                    } else if ((Timer.getFPGATimestamp() - shootTime) > pinTime) {
+                        feed = true;
+                    }                                                 
                     break;
                 default:
-                    shooterOn = false;
-                    shooting = false;
+                    pin = false;
+                    feed = false;
+                    spin = false;
             }
 
             /* Set outputs */
-            shooterFeed.set(shooting || feedTest);
+            shooterFeed.set(feed || feedTest);
+            shooterPin.set(pin || feedTest);
  
-            if (shooterOn || spinTest) {
+            if (spin || spinTest) {
                 shooterControl1.enable();
                 shooterControl2.enable();
             } else {
@@ -156,6 +165,7 @@ public class Shooter extends Thread {
             SmartDashboard.putNumber("Shooter 2 Cmd", shooterMotor2.shooterMotor.get());
             
             SmartDashboard.putBoolean("Shooter feed", shooterFeed.get());
+            SmartDashboard.putBoolean("Shooter pin", shooterPin.get());            
             
             
             /* Sleep for a while */
@@ -172,6 +182,22 @@ public class Shooter extends Thread {
         }
     } 
     
+    public synchronized void SetElevated(boolean e) {
+        if(e != elevated) {
+            //Change detected
+            elevated = e;        
+            shooterElevation.set(e);
+            if(e) {
+                shooterControl1.setSetpoint(ElevatedSetpoint1);
+                shooterControl2.setSetpoint(ElevatedSetpoint2);
+            } else {
+                shooterControl1.setSetpoint(NonElevatedSetpoint1);
+                shooterControl2.setSetpoint(NonElevatedSetpoint2);
+            }
+        }
+        
+    }
+    
     public synchronized void SpinTest(boolean test) {
         spinTest = test;
     } 
@@ -179,4 +205,6 @@ public class Shooter extends Thread {
     public synchronized void FeedTest(boolean test) {
         feedTest = test;
     } 
+        
+    
 }
